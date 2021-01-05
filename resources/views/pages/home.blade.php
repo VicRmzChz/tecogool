c23c6389deb65dab885039944f7cdaa2cb7ef48f
VicRmzChz <vicentermzchz@gmail.com>
2021-01-04 23:29:20 -0600

    comments was remove

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,22 +5,11 @@
 @section('content')
 
 
-<!--div class="container">
-  <div class="row align-items-center">
-    <div class="col-lg-12">
-      <h1 class="display-1" id="tecoSize">Teco<span>Gool</span><span class="minianima"></span></h1>
-      <h3>Juega y Gana <span class="animacion"></span></h3>
-      <hr class="my-4">
-      <a class="btn btn-outline-danger text-white btn-md" href="/lista">Ver Lista</a>
-    </div>
-  </div>
-</div -->
-
 <div class="container-fluid maincontent">
   <div class="row">
     <div class="col-md-8 offset-md-2 info">
-      <h1 class="text-center">tecogool</h1>
-      <p class="text-break text-center">dsfsdjfhkdsjhfkjhsdkfjhksdjlkjslfkj
+      <h1 class="text-center">teco<span class="goolcoler">gool</span></h1>
+      <p class="text-break text-center">Estas listo para participar?
       </p>
       <a class="btn btn-md text-center" href="/lista">Ver Lista</a>
     </div>
