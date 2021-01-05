e9fe04f2b785e6b2c853f37aa933b4b5d8ba9570
VicRmzChz <vicentermzchz@gmail.com>
2021-01-04 23:38:11 -0600

    dashboard test

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,11 +1,1 @@
-@extends('layouts.master')
-@section('title', 'Dashboard')
-@section('content')
-<h1>Profile</h1>
-<h2>welcome, {{session('username')}}</h2>
-
-<a href="/logout">Logout</a>
-
-
-
-@endsection
+<h1>Hello world</h1>
